14c8bac4ee684038bcaf8b8ae4add49e48362526
NumanBashir <sportybashir@gmail.com>
2024-02-12 18:23:00 +0100

    own card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,19 +3,19 @@ import "./ProductCard.css";
 
 const ProductCard = () => {
   return (
-    <div className="product-card">
-      <img src="src/assets/vitaminD.webp" className="product-image" />
-      <div className="product-info">
-        <h2 className="product-title">Produkt1</h2>
-        <div className="product-price">
-          <span className="single-price">395,00 kr</span>
-          <div className="quantity">
-            <input type="number" value="2" className="quantity-input" />
-            <span className="total-price">790,00 kr</span>
-          </div>
+    <>
+      <div className="container">
+        <img src="src/assets/vitaminD.webp" className="product-image" />
+        <div className="product-desc">
+          <h2>Produkt1</h2>
+          <span>200kr</span>
+        </div>
+        <div className="quantity">
+          <span>2</span>
+          <p>400kr</p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
