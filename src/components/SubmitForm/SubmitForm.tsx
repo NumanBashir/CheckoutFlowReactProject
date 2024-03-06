e3a89c042e154525a79888c7204f90249c883e30
NumanBashir <sportybashir@gmail.com>
2024-03-06 15:22:47 +0100

    search for city and post nr is found

--- src/components/SubmitForm/SubmitForm.tsx
+++ src/components/SubmitForm/SubmitForm.tsx
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SubmitForm.css";
 
+// Fix type error from API for ZipCodes
+interface ZipCodes {
+  nr: string;
+  navn: string;
+}
+
+// Fix type error from API for Cities
+interface Cities {
+  nr: string;
+  navn: string;
+}
+
 const SubmitForm = () => {
-  fetch("https://api.dataforsyningen.dk/postnumre")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error("Error fetching postal code data:", error);
-    });
+  const [zipCode1, setZipCode1] = useState("");
+  const [city1, setCity1] = useState("");
+  const [zipCode2, setZipCode2] = useState("");
+  const [city2, setCity2] = useState("");
+  const [zipCodes, setZipCodes] = useState<ZipCodes[]>([]);
+  const [cities, setCities] = useState<Cities[]>([]);
+  // TODO: Bug wiff duplicate zipCode and city, change to zipCode1 and zipCode2 etc.
+
+  // Fetch API and set zipCodes to data
+  useEffect(() => {
+    fetch("https://api.dataforsyningen.dk/postnumre")
+      .then((response) => response.json())
+      .then((data) => {
+        setZipCodes(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching postal code data:", error);
+      });
+  }, []);
+
+  // Fetch API and set cities to data
+  useEffect(() => {
+    fetch("https://api.dataforsyningen.dk/postnumre")
+      .then((response) => response.json())
+      .then((data) => {
+        setCities(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching cities data:", error);
+      });
+  }, []);
+
+  // Logic to get matching city name from zipcode
+  useEffect(() => {
+    const matchingPostalCode = zipCodes.find((code) => code.nr === zipCode1);
+    if (matchingPostalCode) {
+      setCity1(matchingPostalCode.navn);
+    } else {
+      setCity1("");
+    }
+  }, [zipCode1, zipCodes]);
+
+  // Logic to get matching zipcode from city name
+  useEffect(() => {
+    const matchingCity = cities.find((code) => code.navn === city2);
+    if (matchingCity) {
+      setZipCode2(matchingCity.nr);
+    } else {
+      setZipCode2("");
+    }
+  }, [city2, cities]);
 
   return (
     <>
@@ -71,15 +126,24 @@ const SubmitForm = () => {
             Postnummer:
           </label>
           <input
-            type="number"
+            type="text"
             id="postnummer1"
             name="postnummer1"
+            value={zipCode1}
+            onChange={(e) => setZipCode1(e.target.value)}
             className="form-input"
           />
           <label htmlFor="by1" className="form-label">
             By:
           </label>
-          <input type="text" id="by1" name="by1" className="form-input" />
+          <input
+            type="text"
+            id="by1"
+            name="by1"
+            value={city1}
+            className="form-input"
+            readOnly
+          />
           <label htmlFor="adresse2" className="form-label">
             Adresse 2:
           </label>
@@ -93,15 +157,24 @@ const SubmitForm = () => {
             Postnummer:
           </label>
           <input
-            type="number"
+            type="text"
             id="postnummer2"
             name="postnummer2"
             className="form-input"
+            value={zipCode2}
+            readOnly
           />
           <label htmlFor="by2" className="form-label">
             By:
           </label>
-          <input type="text" id="by2" name="by2" className="form-input" />
+          <input
+            type="text"
+            id="by2"
+            name="by2"
+            value={city2}
+            onChange={(e) => setCity2(e.target.value)}
+            className="form-input"
+          />
         </form>
       </div>
     </>
