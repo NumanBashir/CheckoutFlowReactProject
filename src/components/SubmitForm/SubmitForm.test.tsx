06f29038e033bd6518547bb316f4644ea2c11c17
NumanBashir <sportybashir@gmail.com>
2024-03-11 10:03:27 +0100

    test for firstName

--- src/components/SubmitForm/SubmitForm.test.tsx
+++ src/components/SubmitForm/SubmitForm.test.tsx
@@ -6,11 +6,24 @@ describe("SubmitForm", () => {
     render(<SubmitForm />);
 
     const phoneNumberInput = screen.getByLabelText(
-      /telefon:/i
+      /Telefon:/i
     ) as HTMLInputElement;
 
     fireEvent.change(phoneNumberInput, { target: { value: "12345678" } });
 
     expect(phoneNumberInput.value).toBe("12345678");
+  });
+});
+
+describe("SubmitForm", () => {
+  it("updates first name input on change", () => {
+    render(<SubmitForm />);
+
+    const firstNameInput = screen.getByLabelText(
+      /Fornavn:/i
+    ) as HTMLInputElement;
+    fireEvent.change(firstNameInput, { target: { value: "Henrik" } });
+
+    expect(firstNameInput.value).toBe("Henrik");
   });
 });
